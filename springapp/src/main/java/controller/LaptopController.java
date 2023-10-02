d2d55ee56fafd832b3c38f70bd20fd575a2685ec
Iamneo <techadmin@iamneo.ai>
2023-10-02 10:18:14 +0000

    Backup Commit

--- springapp/src/main/java/controller/LaptopController.java
+++ springapp/src/main/java/controller/LaptopController.java
@@ -1,1 +1,17 @@
-package com.examly.springapp.controller
+package com.examly.springapp.controller;
+
+import com.examly.springapp.model.Laptop;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/laptops")
+public class LaptopController{
+    private List<Laptop> laptops = new ArrayList<>();
+
+    @PostMapping
+    public boolean addLaptop(@RequestBody Laptop laptop){
+        
+    }
+}
